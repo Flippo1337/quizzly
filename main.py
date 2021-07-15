750a8ec998686b7681a3c296af450126e2ea67ac
DemiTownson <56600349+DemiTownson@users.noreply.github.com>
2021-07-15 15:37:45 +0100

    cosmetic

--- main.py
+++ main.py
@@ -19,18 +19,21 @@ def index():
 
     return render_template('index.html', title='Welcome', username=name, question=question, correct_answer=correct_answer, wrong_answers=wrong_answers)
 
+
 @app.route('/dashboard/<name>')
 def dashboard(name):
-   return 'welcome %s' % name
+    return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-   if request.method == 'POST':
-      user = request.form['name']
-      return redirect(url_for('dashboard',name = user))
-   else:
-      user = request.args.get('name')
-      return render_template('login.html')
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('dashboard',name = user))
+    else:
+        user = request.args.get('name')
+        return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81, debug=True)
